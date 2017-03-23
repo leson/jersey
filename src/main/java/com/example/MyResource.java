1e0e8fa5a829b2c53a61d1fe0b49c663ec0f7063
leson <i.leson@163.com>
2017-03-23 23:55:30 +0800

    add transfer parameter method

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -22,4 +26,20 @@ public class MyResource {
     public String getIt() {
         return "Got it!";
     }
+    
+    @GET  
+    @Path("/getInfo")  
+    @Produces(MediaType.TEXT_PLAIN)  
+    public String getWebServiceInfo() {  
+        return "Hello,RESTful web service!";  
+    }  
+      
+  
+    @GET  
+    @Path("/{parameter}")  
+    public Response respondMessage(@PathParam("parameter") String parameter,  
+            @DefaultValue("Nothing to say") @QueryParam("value") String value) {  
+        String out = "Hello from " + parameter + ":" + value;  
+        return Response.status(200).entity(out).build();  
+    }  
 }
